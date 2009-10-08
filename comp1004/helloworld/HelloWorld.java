a6bbc4a2e4d991b3ceac256dd8ddf4f1d61ba34b
Alastair Lynn <arplynn@gmail.com>
2009-10-08 11:49:45 +0100

    Hello, world!
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

---
+++ comp1004/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * This is a class to print Hello World when main is called.
+ * Of great practical use to all.
+ */
+public class HelloWorld
+{
+	/**
+	 * Main entry point
+	 */
+	public static void main (String[] args)
+	{
+		System.out.println("Hello, world!");
+	}
+}
